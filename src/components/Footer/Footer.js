23e74927dfd6e761720691d288d0054ff3eae9a5
ConnerKT <connerthompson121@gmail.com>
2024-06-27 14:12:25 -0500

    underline style footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
   return (
     <div className="fixed bottom-0 w-full py-4">
       <footer className="container mx-auto flex justify-center">
         <h1>
-          Created by <a href="https://www.linkedin.com/in/connerkt/">Conner</a>
+          Created by <a class="footer-link" href="https://www.linkedin.com/in/connerkt/">Conner</a>
         </h1>
       </footer>
     </div>
